a34e1dadf94318991263e154c1f5ebf71b14342c
ktrixs <kayitarethiers20@gmail.com>
2019-10-23 11:13:55 +0200

    feat(nodeSetup): Creating a node env

---
+++ server/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const path = require('path');
+const chalk = require('chalk');
+const debug = require('debug')('app');
+const morgan = require('morgan');
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.use(morgan('tiny'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../UI/index.html'));
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  }
+  debug(`running on port ${chalk.green(port)}`);
+});
