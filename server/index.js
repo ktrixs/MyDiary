31e69aeefe1e0a7c7cbf40759f38bc5580928bb8
ktrixs <kayitarethiers20@gmail.com>
2019-10-25 15:48:40 +0200

    feat(apiGetAllEntries): Creating an api to get all notes of the user feature - respond to GET /entries request with all user entries - catch invalid requests and respond with an appropriate message - Test against postman [Finishes #169337501]

--- server/index.js
+++ server/index.js
@@ -1,38 +1,25 @@
+const http = require('http');
 const express = require('express');
 const path = require('path');
 const chalk = require('chalk');
 const debug = require('debug')('app');
 const morgan = require('morgan');
+const itemsRouter = require('./routes/items');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
 app.use(morgan('tiny'));
 app.use(express.static(path.join(__dirname, '../UI')));
-
+app.use('/api/v1/entries', itemsRouter);
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, '../UI/index.html'));
 });
 
-app.get('/users', (req, res) => {
-  res.json([
-    {
-      id: 1,
-      firstName: 'Kayitare',
-      lastName: 'Thierry',
-      email: 'kayitarethiers20@gmail.com'
-    },
-    { id: 2, firstName: 'Gatesi', lastName: 'Angel', email: 'kayit@gmail.com' },
-    {
-      id: 3,
-      firstName: 'Manzi',
-      lastName: 'Gendeon',
-      email: 'kayitare@gmail.com'
-    }
-  ]);
-});
+const server = http.createServer(app);
 
-app.listen(port, err => {
+server.listen(port, err => {
   if (err) {
     console.log(err);
   }
